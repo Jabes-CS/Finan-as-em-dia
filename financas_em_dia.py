d8a2f30e597d51a89a0ac9289897fec5c69242a2
Jabes <jabes.candido!@gmail.com>
2024-02-20 17:14:44 -0300

    Adicionando o código dentro de uma função.

--- financas_em_dia.py
+++ financas_em_dia.py
@@ -1,16 +1,21 @@
-salario = float(input("Digite seu salário: \n"))
-limite_do_cartao_de_credito = int(input("Digite seu saldo de crédito disponível pelo banco: \n"))
-compras = 0
-valores_gastos_no_mes = 0
+def main():
+    
+    salario = float(input("Digite seu salário: \n"))
+    limite_do_cartao_de_credito = int(input("Digite seu saldo de crédito disponível pelo banco: \n"))
+    compras = 0
+    valores_gastos_no_mes = 0
 
-print("O total de gastos até o momento está no valor de R${}".format(valores_gastos_no_mes))
+    print("O total de gastos até o momento está no valor de R${}".format(valores_gastos_no_mes))
 
-while valores_gastos_no_mes <= salario:
-    compras = float(input("Digite o valor da compra: \n"))
-    valores_gastos_no_mes = valores_gastos_no_mes + compras
-    print("\nO total de gastos está no valor de R${}".format(valores_gastos_no_mes))
+    while valores_gastos_no_mes <= salario:
+        compras = float(input("Digite o valor da compra: \n"))
+        valores_gastos_no_mes = valores_gastos_no_mes + compras
+        print("\nO total de gastos está no valor de R${}".format(valores_gastos_no_mes))
 
-    if valores_gastos_no_mes > salario:
-        saldo_negativo = valores_gastos_no_mes - salario
-        print("O valor gasto (R${}) ultrapassa seu salário de (R${}) em (R${})".format(valores_gastos_no_mes, salario, saldo_negativo))
-        print("Você está com credito negativado em R${}".format(saldo_negativo))
+        if valores_gastos_no_mes > salario:
+            saldo_negativo = valores_gastos_no_mes - salario
+            print("O valor gasto (R${}) ultrapassa seu salário de (R${}) em (R${})".format(valores_gastos_no_mes, salario, saldo_negativo))
+            print("Você está com credito negativado em R${}".format(saldo_negativo))
+
+if __name__ == "__main__":
+    main()
